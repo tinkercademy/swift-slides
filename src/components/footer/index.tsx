39c136052751e6bb14593c8ecdff25993beac530
Ryan Theodore The <ryan.the.2006@gmail.com>
2025-02-13 04:06:21 +0000

    Update Footer links to use next/link

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 export function Footer() {
-    return (
-        <footer className={styles.footer}>
-          <img className={styles.tinkerLogo} src="/assets/tinkercademy-long.png" />
-          <br />
-          <div className={styles.appleBadges}>
-            <Image src="/assets/apple-badges.svg" fill={true} alt="Apple badges for Certified Trainer (App Development with Swift), Consultants Network and Professional Learning Specialist." />
-          </div>
-          <br />
-          <p>
-            Find out more:{" "}
-            <a href="https://swiftexplorers.sg">Swift Explorers Challenge</a>{" • "}
-            <a href="https://swiftinsg.org">Swift Accelerator</a>{" • "}
-            <a href="https://tinkercademy.com">Tinkercademy</a>
-            <br />
-            Open-sourced on <a href="https://github.com/tinkercademy/swift-slides">GitHub</a>.{" "}
-            <a href="https://github.com/tinkercademy/swift-slides/issues/new">Report issues</a>.
-            <br />
-            Copyright © 2025 <a href="https://tinkertanker.com">Tinkertanker Pte Ltd</a>. All Rights Reserved.
-          </p>
-        </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+      <Link href="https://tinkercademy.com/" target="_blank">
+        <img className={styles.tinkerLogo} src="/assets/tinkercademy-long.png" />
+      </Link>
+      <br />
+      <div className={styles.appleBadges}>
+        <Image src="/assets/apple-badges.svg" fill={true} alt="Apple badges for Certified Trainer (App Development with Swift), Consultants Network and Professional Learning Specialist." />
+      </div>
+      <br />
+      <p>
+        Find out more:{" "}
+        <Link href="https://swiftexplorers.sg">Swift Explorers Challenge</Link>{" • "}
+        <Link href="https://swiftinsg.org">Swift LinkAccelerator</Link>{" • "}
+        <Link href="https://tinkercademy.com">Tinkercademy</Link>
+        <br />
+        Open-sourced on <Link href="https://github.com/tinkercademy/swift-slides">GitHub</Link>.{" "}
+        <Link href="https://github.com/tinkercademy/swift-slides/issues/new">Report issues</Link>.
+        <br />
+        Copyright © 2025 <Link href="https://tinkertanker.com">Tinkertanker Pte Ltd</Link>. All Rights Reserved.
+      </p>
+    </footer>
+  )
 }
