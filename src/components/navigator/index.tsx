27a2e7c4f69f83590c15e4d6a4374cd2e0d9ddd7
Ryan Theodore The <ryan.the.2006@gmail.com>
2025-02-28 07:03:38 +0000

    Update Navigator bar

--- src/components/navigator/index.tsx
+++ src/components/navigator/index.tsx
@@ -1,15 +1,42 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import { ActionsBar } from "../actionsBar";
+import { FaMoon, FaSun } from "react-icons/fa6";
+import { useDarkMode } from "usehooks-ts";
 
 export function Navigator() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { toggle: toggleDarkMode, isDarkMode } = useDarkMode()
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav>
+    <nav className={`${styles.navigator} ${isScrolled && styles.scrolled}`}>
       <div className={styles.explorersLogo}>
         <Link href="/tracks">
           <Image src="/assets/logo_yellow_540h.png" fill={true} alt="The Swift Explorers Logo" />
         </Link>
       </div>
+      <ActionsBar actions={[
+        {
+          name: "theme",
+          onClick: () => { toggleDarkMode() },
+          icon: (isDarkMode ? <FaSun /> : <FaMoon />)
+        }
+      ]} />
     </nav>
-  )
+  );
 }
