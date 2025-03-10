9ebd89a47d72bd616466cb3c9abc8eaa9a194a54
Ryan Theodore The <ryan.the.2006@gmail.com>
2025-03-10 02:47:57 +0000

    Add legacy redirect documentation

--- next.config.ts
+++ next.config.ts
@@ -8,13 +8,15 @@ const nextConfig: NextConfig = {
         destination: "/tracks",
         permanent: false
       },
+      // Temporary redirects for legacy presentation links from old HTML website (additional workaround code in src/app/tracks/[trackId]/page.tsx)
+      // https://explore.swiftin.sg/codingclub -> https://explore.swiftin.sg/tracks
+      // https://explore.swiftin.sg/codingclub/a -> https://explore.swiftin.sg/tracks/track_a
+      // https://explore.swiftin.sg/codingclub/a/presentation.html?deck=apple-keynote#/slide-view -> https://explore.swiftin.sg/tracks/track_a/unit_01#/slide-view
       {
         source: "/codingclub",
         destination: "/tracks",
         permanent: false
       },
-      // https://explore.swiftin.sg/codingclub/a -> https://explore.swiftin.sg/tracks/track_a
-      // https://explore.swiftin.sg/codingclub/a/presentation.html?deck=apple-keynote#/slide-view -> https://explore.swiftin.sg/tracks/track_a/unit_01#/slide-view
       {
         source: "/codingclub/:track([abcx])/:presentation*",
         destination: "/tracks/track_:track",
