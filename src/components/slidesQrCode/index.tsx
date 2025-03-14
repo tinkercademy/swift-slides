794206a790232a609b4d7776616bf2892a865a61
Ryan Theodore The <ryan.the.2006@gmail.com>
2025-03-14 06:46:31 +0000

    Update QR code to be a link and remove hash/search from URL

--- src/components/slidesQrCode/index.tsx
+++ src/components/slidesQrCode/index.tsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
+import { useEffect, useState } from "react";
+import Link from "next/link";
 import QRCode from "react-qr-code";
 
-import styles from "./styles.module.scss"
-import { useEffect, useState } from "react";
+import styles from "./styles.module.scss";
 
 // TODO: figure out of old method of resolveCurrentURL may still work in production, then removing the need for this Client Component wrapper
 // (previous implementation using purely Server Components did not work on production, only worked on development)
@@ -16,22 +17,27 @@ import { useEffect, useState } from "react";
 // }
 
 export function SlidesQRCode() {
-    const [currentURL, setCurrentURL] = useState("");
+  const [currentURL, setCurrentURL] = useState("");
 
-    useEffect(() => {
-        if (process) {
-            setCurrentURL(window.location.href);
-        }
-    }, []);
+  useEffect(() => {
+    if (process) {
+      const url = new URL(window.location.href);
+      url.hash = "";
+      url.search = "";
+      setCurrentURL(url.toString());
+    }
+  }, []);
 
-    return (
-        <div className={styles.border}>
-            <QRCode
-                className={styles.qrCode}
-                size={256}
-                value={currentURL}
-                viewBox={`0 0 256 256`}
-            />
-        </div>
-    )
+  return (
+    <div className={styles.border}>
+      <Link href={currentURL}>
+        <QRCode
+          className={styles.qrCode}
+          size={256}
+          value={currentURL}
+          viewBox={`0 0 256 256`}
+        />
+      </Link>
+    </div>
+  );
 }
